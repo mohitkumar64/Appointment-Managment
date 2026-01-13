52938d089ee53587ec0800ad8c0427fea9567e4a
Mohit kumar <mk7004048@gmail.com>
2026-01-14 00:23:32 +0530

    backhand error

--- Back-end/Router/Authroute.js
+++ Back-end/Router/Authroute.js
@@ -141,8 +141,11 @@ Router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+    
+
+    const user = await User.findOne({ email }).select('+password');
     if (!user || !user.password) {
+     
       return res.status(401).json({ error: "Invalid credentials" });
     }
 
